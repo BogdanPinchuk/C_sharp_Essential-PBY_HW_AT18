7b20772a40fc69ddcd05436b9ab6016f7847eb96
Bogdan <pinchuk.brus@gmail.com>
2019-08-12 13:42:01 +0300

    Add LesApp1

---
+++ LesApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibClass = MyLibrary.LibClass;
+
+namespace LesApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // join unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // рандомні числа
+            Random rnd = new Random();
+
+            // виконання через підключену бібліотеку
+            LibClass.LibClassMethod(rnd.Next(0, short.MaxValue),
+                rnd.Next(0, short.MaxValue));
+
+            // delay
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ MyLibrary/LibClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Моя бібліотека
+/// </summary>
+namespace MyLibrary
+{
+    /// <summary>
+    /// Бібліотечний клас
+    /// </summary>
+    public static class LibClass
+    {
+        public static void LibClassMethod(int a, int b)
+        {
+            Console.WriteLine($"\n\t{a:N0} + {b:N0} = {a + b:N0}");
+        }
+    }
+}
